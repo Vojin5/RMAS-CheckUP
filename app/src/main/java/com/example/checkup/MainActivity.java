0608d3f274f5da3e3f9fee0f08b7a85896b0960b
Vojin Minic <vojinminic@elfak.rs>
2023-04-15 11:57:33 +0200

    Login Page v1

---
+++ app/src/main/java/com/example/checkup/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.checkup;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    FirebaseDatabase firebaseDatabase;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //FIREBASE SETUP
+        firebaseDatabase = FirebaseDatabase.getInstance("https://checkup-f6ce4-default-rtdb.europe-west1.firebasedatabase.app");
+
+
+    }
+}
